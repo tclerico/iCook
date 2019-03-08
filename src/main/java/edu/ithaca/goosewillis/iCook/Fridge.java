d73b91aba2734a594f65b1d777f11c3465e854b2
Colin Hay <chay@ithaca.edu>
2019-03-08 10:25:06 -0500

    created fridge class

--- src/main/java/edu/ithaca/goosewillis/iCook/Fridge.java
+++ src/main/java/edu/ithaca/goosewillis/iCook/Fridge.java
@@ -1,4 +1,10 @@
 package edu.ithaca.goosewillis.iCook;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Fridge {
+    private List<String> ingredients = new ArrayList<String>();
+
 }
