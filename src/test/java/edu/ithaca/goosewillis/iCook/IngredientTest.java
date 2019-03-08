793e99aaeb71160e58644bf388b90dbb18e69f15
jackcutri <jcutri@ithaca.edu>
2019-03-08 12:52:15 -0500

    Ingredient class and tests

---
+++ src/test/java/edu/ithaca/goosewillis/iCook/IngredientTest.java
@@ -0,0 +1,24 @@
+package edu.ithaca.goosewillis.iCook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IngredientTest {
+
+    @Test
+    void constructorTest(){
+        Ingredient ingredient1 = new Ingredient("Chicken Breast", 4);
+
+        assertEquals("Chicken Breast", ingredient1.getName());
+        assertEquals(4, ingredient1.getCount());
+
+        ingredient1.setName("Chicken Wing");
+        ingredient1.setCount(12);
+        assertEquals("Chicken Wing", ingredient1.getName());
+        assertEquals(12, ingredient1.getCount());
+
+        assertThrows(IllegalArgumentException.class, ()-> new Ingredient("Sauerkraut", 0));
+        assertThrows(IllegalArgumentException.class, ()-> new Ingredient("Sauerkraut", -5));
+    }
+}
