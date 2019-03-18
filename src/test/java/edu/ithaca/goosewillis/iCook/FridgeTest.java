b752801d20b275f78f8e6f23bbb7197758751152
Colin Hay <chay@ithaca.edu>
2019-03-18 02:05:05 -0400

    added test for getIngredients

--- src/test/java/edu/ithaca/goosewillis/iCook/FridgeTest.java
+++ src/test/java/edu/ithaca/goosewillis/iCook/FridgeTest.java
@@ -1,4 +1,27 @@
 package edu.ithaca.goosewillis.iCook;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FridgeTest {
+
+    @Test
+    void getIngredientsTest() {
+        Ingredient testIngredient1 = new Ingredient("testIngredient1", 1);
+        Ingredient testIngredient2 = new Ingredient("testIngredient2", 2);
+        Ingredient testIngredient3 = new Ingredient("testIngredient3", 3);
+
+        List<Ingredient> testIngredients = new ArrayList<Ingredient>();
+        testIngredients.add(testIngredient1);
+        testIngredients.add(testIngredient2);
+        testIngredients.add(testIngredient3);
+
+        Fridge testFridge = new Fridge(testIngredients);
+
+        Assert.assertEquals(testIngredients, testFridge.getIngredients());
+    }
+
 }
