e3459487fc094f5cdc06c6ac7b591879ada5fb63
jackcutri <jcutri@ithaca.edu>
2019-03-20 19:59:52 -0400

    Added cookTime to Ingredient constructor for one-tray purposes, and made failing tests

--- src/main/java/edu/ithaca/goosewillis/iCook/Ingredient.java
+++ src/main/java/edu/ithaca/goosewillis/iCook/Ingredient.java
@@ -4,17 +4,18 @@ public class Ingredient {
 
     public String name;
     public double count;
+    public double cookTime;  //time for ingredient to cook for one-tray-meal at 400 degrees
     // will add appropriate tags (Vegan, Vegetarian, etc.) after API stuff
-    // need to discuss with client about ovenTime
 
     public Ingredient(){}
 
-    public Ingredient(String name, double count){
-        if(count>0){
+    public Ingredient(String name, double count, double cookTime){
+        if(count>0 && cookTime>0 && !name.equals("") && !name.equals(" ")){
             this.name=name;
             this.count=count;
+            this.cookTime=cookTime;
         }else{
-            throw new IllegalArgumentException(count+"is not a valid ingredient count");
+            throw new IllegalArgumentException("Please enter valid ingredient inputs.");
         }
     }
 
@@ -22,7 +23,11 @@ public class Ingredient {
 
     public double getCount(){return count;}
 
+    public double getCookTime(){return 0;}
+
     public void setName(String name){this.name=name;}
 
-    public void setCount(int count){this.count=count;}
+    public void setCount(double count){this.count=count;}
+
+    public void setCookTime(double cookTime){this.cookTime=0;}
 }
